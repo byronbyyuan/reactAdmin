02d0085acc481643f3725f63f67c85582d7f4627
y_by <14138993@qq.com>
2018-12-20 18:13:19 +0800

    依赖判断

--- dev_start.bat
+++ dev_start.bat
@@ -1,5 +1,5 @@
 @echo off
-title 环境启动
+title ??????
 color 03
 mode con cols=115 lines=35
 SET SourceFile=\node_modules
@@ -13,7 +13,7 @@ Echo                                         开发环境启动中..
 ECHO.
 Echo                  ==========================================================================
 echo.
-echo 查询依赖中
+echo 依赖查询中
 echo.
 if exist %cd%%SourceFile% (
     echo 依赖已存在，启动中
@@ -21,7 +21,7 @@ if exist %cd%%SourceFile% (
     echo 依赖不存在，开始下载依赖
     echo.
     npm i
-    echo 依赖下载完成，环境启动中    
+    echo 依赖下载完成，环境启动中   
 )
 
 @echo off
